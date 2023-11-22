1455d173633f3f1fab30e7d11aaeccdf20059399
William Engman <william.engman@hotmail.com>
2023-11-22 21:23:49 +0100

    telescope working

--- nvim/lua/wlem/config/keybinds.lua
+++ nvim/lua/wlem/config/keybinds.lua
@@ -1,7 +1,8 @@
 -- set leader key to space
---vim.g.mapleader = " "
+vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
+
 
 ---------------------
 -- General Keymaps -------------------
@@ -20,8 +21,8 @@ local keymap = vim.keymap -- for conciseness
 --keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
---keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
---keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 --keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 --keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
