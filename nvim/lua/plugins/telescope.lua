b4a2f864622bb2d857fa2f89191f8adc7e7477b0
William A Engman <william.a.engman@tietoevry.com>
2024-02-23 13:43:56 +0100

    bufferline removed

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,7 +4,8 @@ return {
   dependencies = { 
     'nvim-lua/plenary.nvim',
     { "nvim-telescope/telescope-fzf-native.nvim", build='make'},
-    "nvim-tree/nvim-web-devicons"
+    "nvim-tree/nvim-web-devicons",
+    "tiagovla/scope.nvim"
   },
   config = function()
     local telescope = require("telescope")
@@ -23,12 +24,14 @@ return {
     })
 
     telescope.load_extension("fzf")
+    telescope.load_extension("scope")
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>b", "<cmd>Telescope buffers<cr>", { desc = "Find in open buffers" })
    -- keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
    -- keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
   end,
