6cb241d0dd7fee489882ad27f744f26b6790e03b
William A Engman <william.a.engman@tietoevry.com>
2024-03-08 08:24:39 +0100

    behavioural chagnes

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -13,8 +13,35 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins",
   {
+    checker = {
+      enabled = true,
+      frequency = 86400,
+    },
     change_detection = { 
       notify = false,
-    }
-  }
+    },
+     custom_keys = {
+      -- You can define custom key maps here. If present, the description will
+      -- be shown in the help menu.
+      -- To disable one of the defaults, set it to false.
+
+      ["<leader>l"] = {
+        function(plugin)
+          require("lazy.util").float_term({ "lazygit", "log" }, {
+            cwd = plugin.dir,
+          })
+        end,
+        desc = "Open lazygit log",
+      },
+
+      ["<leader>t"] = {
+        function(plugin)
+          require("lazy.util").float_term(nil, {
+            cwd = plugin.dir,
+          })
+        end,
+        desc = "Open terminal in plugin dir",
+      },
+    },
+  },
 )
