ccf7812892ef0bdaa1db09eb5bf39c980b3e894b
William A Engman <william.a.engman@tietoevry.com>
2024-07-18 11:27:46 +0200

    hide file info in neotree

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -11,6 +11,24 @@ return {
 
 		require("neo-tree").setup({
 			close_if_last_window = true,
+      default_component_configs = {
+          file_size = {
+            enabled = false,
+            required_width = 64, -- min width of window required to show this column
+          },
+          type = {
+            enabled = false,
+            required_width = 122, -- min width of window required to show this column
+          },
+          last_modified = {
+            enabled = false,
+            required_width = 88, -- min width of window required to show this column
+          },
+          created = {
+            enabled = false,
+            required_width = 110, -- min width of window required to show this column
+          },
+        },
 			event_handlers = {
 				--[[ {
 					event = "file_opened",
@@ -41,6 +59,7 @@ return {
 				},
 			},
 			window = {
+        auto_expand_width = true,
 				position = "right",
 			},
 			filesystem = {
