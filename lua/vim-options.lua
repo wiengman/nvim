0b9f0f2d96590f74cc16f5e2713ef202eb48b422
William Engman <william.engman@hotmail.com>
2024-07-29 14:32:12 +0200

    cmdline hide when inactive

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -31,5 +31,17 @@ vim.g.mapleader = " "
 
 -- Yank to default clipboard
 options.clipboard:append("unnamedplus")
-vim.keymap.set({'n', 'v'}, 'd', '"_d', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, 'd', '"_d', { noremap = true })
 vim.keymap.set('n', 'dd', '"_dd', { noremap = true })
+
+
+vim.api.nvim_create_autocmd({ "CmdlineEnter" }, {
+  pattern = { "*" },
+  command = "set cmdheight=1"
+})
+
+vim.api.nvim_create_autocmd({ "CmdlineLeave" }, {
+  pattern = { "*" },
+  command = "set cmdheight=0"
+})
+
