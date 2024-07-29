6fc07cb98a0d7fb320a523d28f98f11e611cfaa0
William Engman <william.engman@hotmail.com>
2024-07-29 14:11:53 +0200

    cleanup

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,31 +1,33 @@
+local unsaved_buffers = function()
+  local unsaved_count = 0
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_get_option(buf, "modified") then
+      unsaved_count = unsaved_count + 1
+    end
+  end
+  if unsaved_count > 0 then
+    return string.format("%d Unsaved Buffers", unsaved_count)
+  else
+    return ""
+  end
+end
+
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				disabled_filetypes = {
-					statusline = { "neo-tree", "neo-tree-popup" },
-					winbar = { "neo-tree", "neo-tree-popup" },
-				},
-			},
-			sections = {
-				lualine_c = {
-					"filename",
-					function()
-						local unsaved_count = 0
-						for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-							if vim.api.nvim_buf_get_option(buf, "modified") then
-								unsaved_count = unsaved_count + 1
-							end
-						end
-						if unsaved_count > 0 then
-							return string.format("%d Unsaved Buffers", unsaved_count)
-						else
-							return ""
-						end
-					end,
-				},
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+      options = {
+        disabled_filetypes = {
+          statusline = { "neo-tree", "neo-tree-popup" },
+          winbar = { "neo-tree", "neo-tree-popup" },
+        },
+      },
+      sections = {
+        lualine_c = {
+          "filename",
+          unsaved_buffers,
+        },
+      },
+    })
+  end,
 }
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,7 +1,7 @@
 return {
-	"MeanderingProgrammer/markdown.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-	config = function()
-		require("render-markdown").setup({})
-	end,
+  "MeanderingProgrammer/markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("render-markdown").setup({})
+  end,
 }
