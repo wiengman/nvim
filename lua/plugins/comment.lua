0e962f2631bea2afbb525c04d6ed2d2c56081627
William Engman <william.engman@hotmail.com>
2024-08-06 14:24:55 +0200

    new keymaps for comment

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,6 +1,28 @@
 return {
   "numToStr/Comment.nvim",
   config = function()
-    require("Comment").setup()
+    require("Comment").setup({
+      toggler = {
+        --Line-Comment toggle keymap
+        line = "<leader>cc",
+        block = "<leader>cb",
+      },
+      extra = {
+        above = "<leader>ca",
+        below = "<leader>cd",
+        eol = "<leader>ce",
+      },
+      opleader = {
+
+        line = nil,
+        block = nil,
+      },
+      mappings = {
+        ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+        basic = true,
+        ---Extra mapping; `gco`, `gcO`, `gcA`
+        extra = true,
+      },
+    })
   end
 }
