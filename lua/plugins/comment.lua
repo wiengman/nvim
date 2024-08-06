a60083765f591b0681ab245caf9413f15dcb241c
William Engman <william.engman@hotmail.com>
2024-08-06 15:18:09 +0200

    new keymaps for comment

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -4,16 +4,16 @@ return {
     require("Comment").setup({
       toggler = {
         --Line-Comment toggle keymap
-        line = "<leader>cc",
-        block = "<leader>cb",
+        line = nil,
+        block = nil,
       },
       extra = {
-        above = "<leader>ca",
-        below = "<leader>cd",
-        eol = "<leader>ce",
+        above = nil,
+        below = nil,
+        eol = nil,
       },
       opleader = {
-
+        --
         line = nil,
         block = nil,
       },
@@ -24,5 +24,16 @@ return {
         extra = true,
       },
     })
+
+    vim.keymap.set("n", "<leader>cc", "<Plug>(comment_toggle_linewise_current)", { desc = "[cc]omment" })
+    vim.keymap.set("x", "<leader>cc", "<Plug>(comment_toggle_linewise_visual))", { desc = "[cc]omment (Visual)" })
+    vim.keymap.set("n", "<leader>cb", "<Plug>(comment_toggle_linewise_current)", { desc = "[c]omment [b]lockwise" })
+    vim.keymap.set("x", "<leader>cb", "<Plug>(comment_toggle_blockwise_current))",
+      { desc = "[c]omment [b]lockwise (Visual)" })
+
+    local comment_api = require('Comment.api')
+    vim.keymap.set("n", "<leader>ca", comment_api.insert.linewise.above, { desc = "[c]omment [a]bove" })
+    vim.keymap.set("n", "<leader>cd", comment_api.insert.linewise.below, { desc = "[c]omment [d]own" })
+    vim.keymap.set("n", "<leader>ce", comment_api.locked('insert.linewise.eol'), { desc = "[c]omment [e]nd of line" })
   end
 }
