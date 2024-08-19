3bcdfaf4549771938a8bf9ea31c05626437f4559
William Engman <william.engman@hotmail.com>
2024-08-19 08:17:26 +0200

    update dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -3,8 +3,22 @@ return {
   event = 'VimEnter',
   config = function()
     require('dashboard').setup {
-      -- config
+      theme = "doom",
+      config = {
+        center = {
+          {
+            icon = '🛠',
+            icon_hl = 'group',
+            desc = ' Open Neovim configuration files',
+            desc_hl = 'group',
+            key = 's',
+            key_hl = 'group',
+            key_format = ' %s', -- `%s` will be substituted with value of `key`
+            action = "lua vim.api.nvim_command('Neotree dir=' .. vim.fn.stdpath('config'))",
+          },
+        },
+      }
     }
   end,
-  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } }
 }
