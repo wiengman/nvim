ab3c2f0e131ce675c6233166a8b7740a9660d863
William Engman <william.engman@hotmail.com>
2024-09-24 10:04:24 +0200

    update comments color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,5 +4,7 @@ return {
   config = function()
     require("nightfox").setup({})
     vim.cmd("colorscheme nordfox")
+    vim.api.nvim_set_hl(0, "Comment", { fg = "#ffeca5"})
+    vim.api.nvim_set_hl(0, "@comment", { link = "Comment" })
   end,
 }
