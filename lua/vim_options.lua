52072b2ad27144bde3d7b2660dd764fafc7a5663
William Engman <william.engman@hotmail.com>
2024-10-25 08:33:19 +0200

    hl on yank

--- lua/vim_options.lua
+++ lua/vim_options.lua
@@ -37,6 +37,13 @@ options.clipboard:append("unnamedplus")
 vim.keymap.set({ 'n', 'v' }, 'd', '"_d', { noremap = true })
 vim.keymap.set('n', 'dd', '"_dd', { noremap = true })
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
 
 -- hide cmdline when not in use
 --[[ options.cmdheight = 0
