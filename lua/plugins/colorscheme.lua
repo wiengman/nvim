377582b26bf250fa5224d26a8a5995011b90d1cb
William Engman <william.engman@hotmail.com>
2024-11-12 18:18:58 +0100

    prepare for later transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,8 +4,12 @@ return {
   config = function()
     require("nightfox").setup({})
     vim.cmd("colorscheme nordfox")
+
     -- Comment colors
     vim.api.nvim_set_hl(0, "Comment", { fg = "#AFA16D" })
     vim.api.nvim_set_hl(0, "@comment", { link = "Comment" })
+    -- Make Transparent
+    -- vim.api.nvim_set_hl(0, "Normal", { ctermbg = "none" })
+    -- vim.api.nvim_set_hl(0, "NonText", { ctermbg = "none" })
   end,
 }
