3135157698c95bfce6563ce0002e6a786d278dc1
William Engman <william.engman@hotmail.com>
2024-11-30 18:32:28 +0100

    remove yankcmd and commented stuff

--- lua/vim_options.lua
+++ lua/vim_options.lua
@@ -37,26 +37,6 @@ options.clipboard:append("unnamedplus")
 vim.keymap.set({ 'n', 'v' }, 'd', '"_d', { noremap = true })
 vim.keymap.set('n', 'dd', '"_dd', { noremap = true })
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
--- hide cmdline when not in use
---[[ options.cmdheight = 0
-vim.api.nvim_create_autocmd({ "CmdlineEnter" }, {
-  pattern = { "*" },
-  command = "set cmdheight=1"
-})
-
-vim.api.nvim_create_autocmd({ "CmdlineLeave" }, {
-  pattern = { "*" },
-  command = "set cmdheight=0"
-}) ]]
-
 vim.api.nvim_command('command! FilePathCp let @+=expand("%:p")')
 
 -- Dump Neovim colors to terminal colors
