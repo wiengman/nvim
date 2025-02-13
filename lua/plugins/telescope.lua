60115a11a2126f9d8d65a30be9223177c21ee9d1
William Engman <william.engman@hotmail.com>
2025-02-13 22:27:24 +0100

    add extra mappings to ts

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,12 @@
+local function filename(_, path)
+	local tail = vim.fs.basename(path)
+	local parent = vim.fs.dirname(path)
+	if parent == "." then
+		return tail
+	end
+	return string.format("%s\t\t%s", tail, parent)
+end
+
 return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
@@ -12,18 +21,58 @@ return {
 		-- Find buffers
 		-- recently opened files
 		local ts = require("telescope")
-		ts.setup()
-		ts.load_extension("fzf")
-
 		local builtin = require("telescope.builtin")
-		vim.keymap.set(
-			"n",
-			"<leader>ff",
-			function() builtin.find_files({ find_command = { "rg", "--files", "--ignore", "--hidden", "--iglob=!.git/*" } }) end ,
-			{ desc = "Telescope find files" }
-		)
+		local actions = require("telescope.actions")
+		ts.setup({
+			defaults = {
+				vimgrep_arguments = {
+					"rg",
+					"--ignore",
+					"--hidden",
+					"--smart-case",
+					"--column",
+				},
+				file_ignore_patterns = {
+					"^.git/",
+				},
+				layout_config = {
+					width = 0.95,
+					height = 0.95,
+				},
+				mappings = {
+					i = {
+						["<esc>"] = actions.close,
+						["<C-j>"] = actions.move_selection_next,
+						["<C-k>"] = actions.move_selection_previous,
+            ["<C-e>"] = actions.preview_scrolling_down,
+            ["<C-y>"] = actions.preview_scrolling_up,
+						["<C-Space>"] = actions.to_fuzzy_refine,
+            ["<C-h>"] = actions.which_key,
+						["<A-d>"] = actions.delete_buffer,
+					},
+				},
+			},
+
+			pickers = {
+				find_files = {
+					path_display = filename,
+				},
+				oldfiles = {
+					path_display = filename,
+				},
+				buffers = {
+					path_display = filename,
+				},
+				live_grep = {
+					path_display = filename,
+				},
+			},
+		})
+		ts.load_extension("fzf")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 		vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Telescope help tags" })
+		vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Git status" })
 	end,
 }
