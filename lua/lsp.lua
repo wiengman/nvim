8a0e0b85511c9103a5b6281f175eba84eb1beadc
William Engman <william.engman@hotmail.com>
2025-05-08 19:03:49 +0200

    change some diagnostics symbols

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -8,12 +8,12 @@ vim.diagnostic.config({
 	virtual_text = {
 		prefix = function(diagnostic)
 			local icons = {
-				[vim.diagnostic.severity.ERROR] = "",
-				[vim.diagnostic.severity.WARN] = "",
-				[vim.diagnostic.severity.INFO] = "󰋽",
-				[vim.diagnostic.severity.HINT] = "󰤱",
+				[vim.diagnostic.severity.ERROR] = " ",
+				[vim.diagnostic.severity.WARN] = " ",
+				[vim.diagnostic.severity.INFO] = " ",
+				[vim.diagnostic.severity.HINT] = "󰐃 ",
 			}
-			return icons[diagnostic.severity] or ""
+			return icons[diagnostic.severity] or " "
 		end,
 		spacing = 2,
 	},
