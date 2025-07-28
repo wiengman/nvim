16b696bdf6c1f5ba015b4044d93635b6d4d16478
William Engman <william.engman@hotmail.com>
2025-07-28 10:12:30 +0200

    update WinSeparator color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -12,6 +12,7 @@ return {
 		-- Column
 		hl(0, "LineNr", { fg = "#b8b4d0", bg = column_bg })
 		hl(0, "SignColumn", { fg = "#938aa9", bg = column_bg })
+		hl(0, "WinSeparator", { fg = "#2a2a37"})
 
 		-- Diagnostics
 		hl(0, "DiagnosticError", { fg = "#e82424", bg = "#3c2935" })
